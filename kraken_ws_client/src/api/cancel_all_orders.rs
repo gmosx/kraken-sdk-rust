e63eca71f913f181357d187a244d73dafe7b9d02
George Moschovitis <gmosx@reizu.com>
2023-08-27 12:19:13 +0300

    wip

--- kraken_ws_client/src/api/cancel_all_orders.rs
+++ kraken_ws_client/src/api/cancel_all_orders.rs
@@ -1,22 +1,17 @@
-use crate::client::PublicRequest;
-use serde::Serialize;
+//! <https://docs.kraken.com/websockets-v2/#cancel-all-orders>
 
-#[derive(Debug, Serialize)]
-pub struct CancelAllOrdersParams<'a> {
-    /// Session token.
-    pub token: &'a str,
-}
+use crate::client::{PrivateParams, PrivateRequest};
 
 /// Cancels all pending orders.
 ///
 /// <https://docs.kraken.com/websockets-v2/#cancel-all-orders>
-pub type CancelAllOrdersRequest<'a> = PublicRequest<CancelAllOrdersParams<'a>>;
+pub type CancelAllOrdersRequest = PrivateRequest<()>;
 
-impl CancelAllOrdersRequest<'_> {
-    pub fn new(token: &str) -> CancelAllOrdersRequest {
+impl CancelAllOrdersRequest {
+    pub fn new() -> CancelAllOrdersRequest {
         CancelAllOrdersRequest {
             method: "cancel_all".to_owned(),
-            params: CancelAllOrdersParams { token },
+            params: PrivateParams::new(()),
             req_id: None,
         }
     }
