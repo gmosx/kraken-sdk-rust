faea34146297d4e63af82865d537d7c401bb6bbd
Zach Crownover <zachary.crownover@gmail.com>
2024-12-12 00:04:23 +0000

    Allow String from Kraken to be properly cast into local Enums to use throughout

--- kraken_rest_client/src/types/order.rs
+++ kraken_rest_client/src/types/order.rs
@@ -1,4 +1,5 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde::de::Error;
 use std::fmt;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize, Serialize, Ord, PartialOrd, Hash)]
@@ -16,6 +17,23 @@ impl fmt::Display for OrderSide {
 
         write!(f, "{}", order_side)
     }
+}
+
+impl TryFrom<String> for OrderSide {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "sell" => Ok(OrderSide::Sell),
+            "buy" => Ok(OrderSide::Buy),
+            _ => Err(format!("Invalid order type: {}", s)),
+        }
+    }
+}
+
+fn orderside_from_str<'de, D>(deserializer: D) -> Result<OrderSide, D::Error> where D: Deserializer<'de>, {
+    let orderside = String::deserialize(deserializer)?;
+    OrderSide::try_from(orderside).map_err(D::Error::custom)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize, Serialize, Ord, PartialOrd, Hash)]
@@ -52,6 +70,31 @@ impl fmt::Display for OrderType {
     }
 }
 
+impl TryFrom<String> for OrderType {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "market" => Ok(Self::Market),
+            "limit" => Ok(Self::Limit),
+            "iceberg" => Ok(Self::Iceberg),
+            "stop-loss" => Ok(Self::StopLoss),
+            "take-profit" => Ok(Self::TakeProfit),
+            "stop-loss-limit" => Ok(Self::StopLossLimit),
+            "take-profit-limit" => Ok(Self::TakeProfitLimit),
+            "settle-position" => Ok(Self::SettlePosition),
+            "trailing-stop" => Ok(Self::TrailingStop),
+            "trailing-stop-limit" => Ok(Self::TrailingStopLimit),
+            _ => Err(format!("Invalid order type: {}", s)),
+        }
+    }
+}
+
+fn ordertype_from_str<'de, D>(deserializer: D) -> Result<OrderType, D::Error> where D: Deserializer<'de>, {
+    let ordertype = String::deserialize(deserializer)?;
+    OrderType::try_from(ordertype).map_err(D::Error::custom)
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct OrderDescription {
     // pub ordertxid: Option<String>,
@@ -59,7 +102,9 @@ pub struct OrderDescription {
     pub pair: String,
     // pub time: f64,
     #[serde(rename = "type")]
+    #[serde(deserialize_with = "orderside_from_str")]
     pub orderside: OrderSide,
+    #[serde(deserialize_with = "ordertype_from_str")]
     pub ordertype: OrderType,
     pub price: String,
     pub price2: String,
